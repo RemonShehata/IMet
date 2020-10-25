06ee4eba4c49984d6b0bc71c5bedc75d5e700f3f
RemonShehata <RemonGerges94@gmail.com>
2020-10-25 22:02:43 +0200

    Implemented Room database

--- app/src/main/java/com/raywenderlich/android/imet/data/model/People.kt
+++ app/src/main/java/com/raywenderlich/android/imet/data/model/People.kt
@@ -33,12 +33,16 @@
 
 package com.raywenderlich.android.imet.data.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
 data class People(
-    var name: String = "",
-    var metAt: String = "",
-    var contact: String = "",
-    var email: String = "",
-    var facebook: String = "",
-    var twitter: String = "",
-    var id: Int = 0
+        var name: String = "",
+        var metAt: String = "",
+        var contact: String = "",
+        var email: String = "",
+        var facebook: String = "",
+        var twitter: String = "",
+        @PrimaryKey(autoGenerate = true) var id: Int = 0
 )
